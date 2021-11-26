5f14519eee1a4c03a9821384b6bc31cfa3c76307
21074 <21074@bridgend.ac.uk>
2021-11-26 14:29:51 +0000

    complete

--- exercise 3 times.py
+++ exercise 3 times.py
@@ -1,7 +1,7 @@
-def message_three_times():
+def main():
     message = input("Write a message...")
     print (message)
     print (message)
     print (message)
 if __name__ == '__main__':
-    message_three_times()
+    main()
